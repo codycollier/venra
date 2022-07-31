e5e1a17ea1602dd0d3a5cc37e14e4608e26e91f1
Cody Collier <cody@telnet.org>
2022-07-31 12:14:29 -0500

    add basic visit generator

--- venra/__init__.py
+++ venra/__init__.py
@@ -1,4 +1,12 @@
 
+from . import client
+from . import config
+from . import document
+from . import search
+from . import system
+from . import version
+from . import visit
+
 
 stars = """
  ..   . *      .     .   * *. .        . .  .     .  *           `         .   
--- venra/visit.py
+++ venra/visit.py
@@ -7,4 +7,25 @@ from . import client
 from . import config
 
 
+def feed(namespace, doctype, selection=None):
+    """Yield all docs of a given type and selection
+
+    """
+    visit_base_uri = f"{config.vespa_host_app}/document/v1/{namespace}/{doctype}/docid"
+
+    # initial call
+    vc = client.get_vespa_client()
+    vr = vc.get(f"{visit_base_uri}").json()
+
+    # page through results
+    while True:
+        for d in vr.get("documents"):
+            yield d
+        continuation = vr.get("continuation", False)
+        if continuation:
+            vr = vc.get(f"{visit_base_uri}?continuation={continuation}").json()
+        else:
+            break
+
+    return
 
