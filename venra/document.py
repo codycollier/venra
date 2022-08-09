1e8f1b106c6c7c147ea10181796406d809feaae2
Cody Collier <cody@telnet.org>
2022-08-09 09:58:08 -0500

    initial cut of partial update for main operators

--- venra/document.py
+++ venra/document.py
@@ -2,9 +2,38 @@
 
 """
 
+import json
 
 from . import client
 from . import config
 
+
+
+def partial_update(namespace, doctype, docid, operations):
+    """Apply a partial update using the assign, add, or remove operator
+
+    operations = [("assign", "field_name_here", "field_value_here"), ...]
+
+    reference:
+    * https://docs.vespa.ai/en/reference/document-json-format.html
+
+    Example:
+    * https://docs.vespa.ai/en/reference/document-json-format.html#assign-map-field
+    {
+        "update": "id:mynamespace:food::example",
+        "fields": {
+            "my_food_scores{Strawberries}": { "assign": "Delicious!" }
+        }
+    }
+    """
+    base_uri = f"{config.vespa_host_app}/document/v1/{namespace}/{doctype}/{docid}"
+    partial_update = {"fields": {}}
+    for operation, field_name, field_value in operations:
+        partial_update["fields"][field_name] = {operation: field_value}
+
+    vc = client.get_vespa_client()
+    vr = vc.put(f"{base_uri}", data=json.dumps(partial_update))
+
+    return vr
 
 
