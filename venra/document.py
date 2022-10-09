7ae38da368be7190b9eabbd515e89c3aa8a0eb8c
Cody Collier <cody@telnet.org>
2022-10-09 16:27:25 -0500

    more work on document api module

--- venra/document.py
+++ venra/document.py
@@ -1,11 +1,13 @@
 """venra.document
 
-A module for interacting with the Vespa document api.
+A module for interacting with the Vespa /document/v1 api.
 
 
 Related Vespa documentation:
 * https://docs.vespa.ai/en/document-v1-api-guide.html
 * https://docs.vespa.ai/en/reference/document-v1-api-reference.html
+* https://docs.vespa.ai/en/reference/document-json-format.html
+* https://docs.vespa.ai/en/reference/document-json-format.html#document-operations
 
 
 """
@@ -16,8 +18,28 @@ from . import client
 from . import config
 
 
+def get(namespace, doctype, docid):
+    """Retrieve and return a document
 
-def partial_update(namespace, doctype, docid, operations):
+    """
+    base_uri = f"{config.vespa_host_app}/document/v1/{namespace}/{doctype}/docid/{docid}"
+    vc = client.get_vespa_client()
+    doc = vc.get(f"{base_uri}")
+    return doc
+
+
+def put(namespace, doctype, docid, doc):
+    """Add or update a whole document
+
+    """
+    base_uri = f"{config.vespa_host_app}/document/v1/{namespace}/{doctype}/docid/{docid}"
+    vc = client.get_vespa_client()
+    doc_fields = {"fields": doc}
+    vr = vc.post(f"{base_uri}", data=json.dumps(doc_fields))
+    return vr
+
+
+def update(namespace, doctype, docid, operations):
     """Apply a partial update using the assign, add, or remove operator
 
     operations = [("assign", "field_name_here", "field_value_here"), ...]
@@ -42,6 +64,16 @@ def partial_update(namespace, doctype, docid, operations):
     vc = client.get_vespa_client()
     vr = vc.put(f"{base_uri}", data=json.dumps(partial_update))
 
+    return vr
+
+
+def remove(namespace, doctype, docid):
+    """Delete a document
+
+    """
+    base_uri = f"{config.vespa_host_app}/document/v1/{namespace}/{doctype}/docid/{docid}"
+    vc = client.get_vespa_client()
+    vr = vc.delete(f"{base_uri}")
     return vr
 
 
