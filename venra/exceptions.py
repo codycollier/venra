04c73baf7d1ccc1bf608696e9e70cefa9d4b9d19
Cody Collier <cody@telnet.org>
2022-10-11 14:29:58 -0500

    fix exception naming

--- venra/exceptions.py
+++ venra/exceptions.py
@@ -6,18 +6,18 @@ errors, to assist with application control flow.
 """
 
 
-class VespaException(Exception):
+class VenraException(Exception):
     """A base exception for unexpected errors"""
 
 
-class VespaConnectionError(VespaException):
+class VespaConnectionError(VenraException):
     """An error connecting to Vespa"""
 
 
-class VespaRequestError(VespaException):
+class VespaRequestError(VenraException):
     """A general error making a call to Vespa"""
 
 
-class VespaItemDoesNotExist(VespaException):
+class VespaItemDoesNotExist(VenraException):
     """An error for attempted operation on an item which does not exist"""
 
